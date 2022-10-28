c7feb91684221c3695938778ec59e39db9ca3203
VanjaRo <vanjaro43@gmail.com>
2022-10-28 12:55:03 +0300

    feat: added new transaction errors

--- pkg/services/transactions/errors.go
+++ pkg/services/transactions/errors.go
@@ -8,4 +8,12 @@ var (
 	ErrTransactionNotFound = errors.New("transaction not found")
 
 	ErrCantFreezeSameTransactionTwice = errors.New("can't freeze the same transaction twice")
+
+	ErrAmountsDontMatch = errors.New("amounts of frozen and aply don't match")
+
+	ErrEmptyId = errors.New("user_id, service_id and order_id can't be empty")
+
+	ErrCantApplyNotFrozenTransaction = errors.New("can't apply not frozen transaction")
+
+	ErrTransactionAlreadyApplied = errors.New("transaction already applied")
 )
