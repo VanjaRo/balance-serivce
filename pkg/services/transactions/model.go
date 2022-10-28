945b86f7df2311ac6efae7db20c4b60c6ed102c0
VanjaRo <vanjaro43@gmail.com>
2022-10-28 11:10:05 +0300

    fix: updated Transaction struct with UpdatedAt field to sort by time in future

--- pkg/services/transactions/model.go
+++ pkg/services/transactions/model.go
@@ -1,17 +1,20 @@
 package transactions
 
+import "time"
+
 // State field represents the state of the transaction
 // Possible values are:
 // 1. "FROZEN"
 // 2. "APPLIED"
 type Transaction struct {
-	Id        string  `gorm:"primaryKey; autoIncrement" json:"id"`
-	OrderId   string  `json:"order_id"`
-	UserId    string  `json:"user_id"`
-	ServiceId string  `json:"service_id"`
-	Amount    float64 `json:"amount"`
-	State     string  `json:"state"`
-	IsDeposit bool    `json:"type"`
+	Id        string    `gorm:"primaryKey; autoIncrement" json:"id"`
+	OrderId   string    `json:"order_id"`
+	UserId    string    `json:"user_id"`
+	ServiceId string    `json:"service_id"`
+	Amount    float64   `json:"amount"`
+	State     string    `json:"state"`
+	IsDeposit bool      `json:"type"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:milli" json:"update_time"`
 }
 
 // For the deposit transaction, the service_id and order_id fields will be empty
