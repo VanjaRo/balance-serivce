605ed8bbf2f740de270b2004719a9b79c6d67b29
VanjaRo <vanjaro43@gmail.com>
2022-10-28 12:57:26 +0300

    fix: updated repo interface with UpdateTrStatus func and added Apply func to the service

--- pkg/services/transactions/transactions.go
+++ pkg/services/transactions/transactions.go
@@ -6,13 +6,14 @@ import "context"
 type Repo interface {
 	Create(ctx context.Context, transaction Transaction) error
 	GetTrByOrderAndServiceIds(ctx context.Context, orderId, serviceId string) (Transaction, error)
+	UpdateTrStatus(ctx context.Context, t Transaction) error
 }
 
 // Service defines the business logic of users
 type Service interface {
 	Deposit(ctx context.Context, userId string, amount float64) error
 	Freeze(ctx context.Context, userId, orderId, service_id string, amount float64) error
-	Apply(ctx context.Context, serviceId, orderId string) error
+	Apply(ctx context.Context, userId, orderId, service_id string, amount float64) error
 }
 
 type transaction struct {
@@ -48,6 +49,10 @@ func (t *transaction) Freeze(ctx context.Context, userId, orderId, serviceId str
 	if amount <= 0 {
 		return ErrNegativeOrZeroAmount
 	}
+	// check that the ids are not empty
+	if userId == "" || serviceId == "" || orderId == "" {
+		return ErrEmptyId
+	}
 	// it looks like we can uniquely identify a transaction by the orderId and serviceId
 	_, err := t.repo.GetTrByOrderAndServiceIds(ctx, orderId, serviceId)
 	// if the transaction already exists, we can't freeze it again
@@ -58,7 +63,7 @@ func (t *transaction) Freeze(ctx context.Context, userId, orderId, serviceId str
 			OrderId:   orderId,
 			ServiceId: serviceId,
 			Amount:    amount,
-			State:     "FROZEN",
+			State:     TRANSACTION_STATE_FROZEN,
 			IsDeposit: false,
 		}
 		err := t.repo.Create(ctx, transaction)
@@ -70,9 +75,32 @@ func (t *transaction) Freeze(ctx context.Context, userId, orderId, serviceId str
 
 	return ErrCantFreezeSameTransactionTwice
 }
-func (t *transaction) Apply(ctx context.Context, serviceId, orderId string) error {
+func (t *transaction) Apply(ctx context.Context, userId, orderId, serviceId string, amount float64) error {
 	// apply only frozen transactions
 	// check that the ids are not empty
+	if userId == "" || serviceId == "" || orderId == "" {
+		return ErrEmptyId
+	}
+	// get frozen transaction by orderId and serviceId
+	tr, err := t.repo.GetTrByOrderAndServiceIds(ctx, orderId, serviceId)
+	if err != nil {
+		return err
+	}
+	// check that the transaction is frozen
+	if tr.State != TRANSACTION_STATE_FROZEN {
+		return ErrCantApplyNotFrozenTransaction
+	}
+	// check that amount is the same
+	if tr.Amount != amount {
+		return ErrAmountsDontMatch
+	}
+
+	// update the transaction state to applied
+	tr.State = TRANSACTION_STATE_APPLIED
+	err = t.repo.UpdateTrStatus(ctx, tr)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
