0943dcc9eef7ceb8c58151f1da841add0389e7b6
VanjaRo <vanjaro43@gmail.com>
2022-10-28 11:08:53 +0300

    feat: wrote freeze GetTrByOrderAndServiceIds and added some errors relating to Get

--- pkg/services/transactions/errors.go
+++ pkg/services/transactions/errors.go
@@ -4,4 +4,8 @@ import "errors"
 
 var (
 	ErrNegativeOrZeroAmount = errors.New("amount must be positive")
+
+	ErrTransactionNotFound = errors.New("transaction not found")
+
+	ErrCantFreezeSameTransactionTwice = errors.New("can't freeze the same transaction twice")
 )
--- pkg/services/transactions/store/repo.go
+++ pkg/services/transactions/store/repo.go
@@ -22,6 +22,15 @@ func (tr *transactionRepo) Create(ctx context.Context, transaction transactions.
 	return tr.DB.Create(&transaction).Error
 }
 
+func (tr *transactionRepo) GetTrByOrderAndServiceIds(ctx context.Context, orderId, serviceId string) (transactions.Transaction, error) {
+	var transaction transactions.Transaction
+	err := tr.DB.Where("order_id = ? AND service_id = ?", orderId, serviceId).First(&transaction).Error
+	if err != nil {
+		return transactions.Transaction{}, transactions.ErrTransactionNotFound
+	}
+	return transaction, nil
+}
+
 func (tr *transactionRepo) Migrate() error {
 	return tr.DB.AutoMigrate(&transactions.Transaction{})
 }
